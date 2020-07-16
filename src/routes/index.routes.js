15397b84c36bea3ce16a4e69b2cee473ff492729
thanhlong <info@coders.tokyo>
2020-07-16 09:39:00 +0700

    finsh project

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -35,12 +35,12 @@ router.get('/news/:page', (req, res, next) => {
       .skip((perPage * page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
       .limit(perPage)
       .exec((err, products) => {
-        Product.countDocuments((err, count) => { // đếm để tính có bao nhiêu trang
+        Product.countDocuments((err, count) => { // đếm để tính xem có bao nhiêu trang
           if (err) return next(err);
           res.render('product/index_product', {
-            products, 
-            current: page, 
-            pages: Math.ceil(count / perPage)
+            products, // sản phẩm trên một page
+            current: page, // page hiện tại
+            pages: Math.ceil(count / perPage) // tổng số các page
           });
         });
       });
