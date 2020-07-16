ffccd95b0c151b3ea485470e31c0ae5655223d8a
thanhlong <info@coders.tokyo>
2020-07-16 08:29:39 +0700

    finsh project

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -27,19 +27,19 @@ router.get('/', (req, res, next)=>{
 
 // pagination
 router.get('/news/:page', (req, res, next) => {
-    let perPage = 16;
-    let page = req.params.page || 1;
+  let perPage = 16; // số lượng sản phẩm xuất hiện trên 1 page
+    let page = req.params.page || 1; 
   
     Product
-      .find() // finding all documents
-      .skip((perPage * page) - perPage) // in the first page the value of the skip is 0
-      .limit(perPage) 
+      .find() // find tất cả các data
+      .skip((perPage * page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
+      .limit(perPage)
       .exec((err, products) => {
-        Product.countDocuments((err, count) => { // count to calculate the number of pages
+        Product.countDocuments((err, count) => { // đếm để tính có bao nhiêu trang
           if (err) return next(err);
           res.render('product/index_product', {
-            products,
-            current: page,
+            products, 
+            current: page, 
             pages: Math.ceil(count / perPage)
           });
         });
